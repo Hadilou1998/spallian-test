71fd55831c07e555868874ae2688fbfbd0a4a459
Hadilou1998 <hadilissilame@gmail.com>
2023-09-02 11:23:49 +0200

    update package.json package-lock.json src/App.js src/styles.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 import React from "react";
+import './styles.css';
 
-const App = () => {
-  return (
-    <div className="container">
-      <p> Hello World!</p>
-    </div>
-  );
+class App extends React.Component 
+{
+  // Constructor
+  constructor(props)
+  {
+    super(props);
+
+    this.state =
+    {
+      items: [],
+      DataisLoaded: false
+    };
+  }
+
+  // ComponentDidMount
+  componentDidMount()
+  {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((json) => 
+    {
+      this.setState
+      ({
+        items: json,
+        DataisLoaded: true
+      });
+    })
+  }
+  render()
+  {
+    const { DataisLoaded, items } = this.state;
+    if (!DataisLoaded) 
+      return <div><h1>Veuillez patienter s'il vous plait ...</h1></div>;
+
+    return (
+      <div className="container">
+        <h1> Recover users data </h1> 
+        {
+          items.map((item) => (
+            <ol key = { item.id }>
+              Username: { item.username },
+              Lastname: { item.name }
+            </ol>
+          ))
+        }
+        <p> Hello World!</p>
+      </div>
+    );
+  }
 }
 
 export default App;
